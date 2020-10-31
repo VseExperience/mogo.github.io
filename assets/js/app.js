86020aa7bd1a38f60d3e17872a13957e6fa06432
VseExperience <59097012+VseExperience@users.noreply.github.com>
2020-10-31 20:37:29 +0300

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,7 +4,30 @@ $(function() {
         introH = $("#intro").innerHeight(),
         scrollOffset = $(window).scrollTop();
 
+    
+    function getMobileOperatingSystem() {
+      var userAgent = navigator.userAgent || navigator.vendor || window.opera;
 
+          // Windows Phone must come first because its UA also contains "Android"
+        if (/windows phone/i.test(userAgent)) {
+            return "Windows Phone";
+        }
+
+        if (/android/i.test(userAgent)) {
+            return "Android";
+        }
+
+        // iOS detection from: http://stackoverflow.com/a/9039885/177710
+        if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) {
+            return "iOS";
+        }
+
+        return "unknown";
+    }
+    
+    alert(getMobileOperatingSystem())
+    
+    
     /* Fixed Header */
     checkScroll(scrollOffset);
 
